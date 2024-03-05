59a58780af64b833c58f20b4edc2cc3a1d204808
Vicente Albert <vicente.lopez.external@mytheresa.com>
2024-03-05 08:06:34 +0100

    Created a simple spinner

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,13 +1,21 @@
 import {Outlet} from "react-router-dom";
-import {Box, Container, Divider, Typography} from "@mui/material";
+import {Box, CircularProgress, Container, Divider, Grid, Typography} from "@mui/material";
 
 export default function MainLayout() {
+  const [loading, setLoading] = [false];
   return (
     <Container>
       <Box sx={{ my: 4 }}>
-        <Typography variant="h5" component="h1" color="#67A4CE" sx={{ mb: 2, fontWeight: 'bold' }}>
-          Podcaster
-        </Typography>
+        <Grid container sx={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Typography variant="h5" component="h1" color="#67A4CE" sx={{ mb: 2, fontWeight: 'bold' }}>
+            Podcaster
+          </Typography>
+          {
+            loading && (
+              <CircularProgress/>
+            )
+          }
+        </Grid>
         <Divider />
         <Outlet />
       </Box>
